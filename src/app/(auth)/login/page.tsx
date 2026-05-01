bd8cb48521cd9fc3ec260eb060808f1cb32cd4e2
Rakib-dhali~ <rakibdhali343@gmail.com>
2026-05-01 12:54:55 +0600

    feat(auth): implement login and registration forms with better-auth integration
    - Added login form in `src/app/(auth)/login/page.tsx` with email and password authentication.
    - Added registration form in `src/app/(auth)/register/page.tsx` with name, image, email, and password fields.
    - Integrated better-auth for authentication handling in both forms.
    - Created API route for authentication in `src/app/api/auth/[...all]/route.ts`.
    - Set up auth client in `src/lib/auth-client.ts` for managing authentication requests.
    - Configured MongoDB adapter for better-auth in `src/lib/auth.ts`.
    - Updated package.json to include better-auth and mongodb dependencies.
    - Added console log for courses in `src/app/(main)/courses/page.tsx` for debugging purposes.

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,134 @@
+"use client";
 
-const page = () => {
-  return <div>login</div>;
-};
+import { authClient } from "@/lib/auth-client";
+import Link from "next/link";
+import { useState } from "react";
 
-export default page;
+export default function LoginForm() {
+   const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+  
+      const {data, error} = await authClient.signIn.email({
+          email,
+          password,
+          callbackURL: "/"
+      })
+    };
+
+  return (
+    <main className="px-4 md:px-8 min-h-screen flex flex-col items-center justify-center">
+      <div className="max-w-md w-full">
+        <div className="p-6 rounded-lg bg-blue-50/50 border border-blue-300 shadow-xs md:p-6">
+          <h1 className="text-blue-900 text-center text-2xl font-bold">
+            Login to Your Account
+          </h1>
+          <form onSubmit={handleSubmit} className="space-y-4 mt-7">
+            <div>
+              <label
+                htmlFor="email"
+                className="mb-2 text-slate-900 font-medium text-sm inline-block"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="john@readymadeui.com"
+                required
+                className="px-3 py-2.5 text-sm text-slate-900 rounded-md bg-white w-full outline-1 -outline-offset-1 outline-slate-300 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="mb-2 text-slate-900 font-medium text-sm inline-block"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="••••••••"
+                required
+                className="px-3 py-2.5 text-sm text-slate-900 rounded-md bg-white w-full outline-1 -outline-offset-1 outline-slate-300 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600"
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full py-2 px-3.5 text-sm rounded-md font-semibold cursor-pointer tracking-wide text-white border border-blue-600 bg-blue-600 hover:bg-blue-700 transition-all focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500"
+            >
+              Login
+            </button>
+          </form>
+
+          <div className="text-slate-900 text-sm text-center mt-5">
+            Don not have an account?
+            <Link
+              href="/register"
+              className="text-blue-700 hover:underline ml-1 font-medium dark:text-blue-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 rounded"
+            >
+              Register Now
+            </Link>
+          </div>
+
+          <div className="my-8 flex items-center gap-4">
+            <hr className="w-full border-slate-300 dark:border-neutral-700" />
+            <p className="text-lg font-bold text-blue-700 text-center">or</p>
+            <hr className="w-full border-slate-300 dark:border-neutral-700" />
+         </div>
+          <div>
+            <button className="w-full flex items-center justify-center gap-2.5 mt-3 py-2 px-3.5 text-sm rounded-md font-semibold text-slate-900 border border-slate-300 bg-white hover:bg-gray-50 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-4.5"
+                viewBox="0 0 512 512"
+                aria-hidden="true"
+              >
+                <path
+                  fill="#fbbd00"
+                  d="M120 256c0-25.367 6.989-49.13 19.131-69.477v-86.308H52.823C18.568 144.703 0 198.922 0 256s18.568 111.297 52.823 155.785h86.308v-86.308C126.989 305.13 120 281.367 120 256z"
+                  data-original="#fbbd00"
+                />
+                <path
+                  fill="#0f9d58"
+                  d="m256 392-60 60 60 60c57.079 0 111.297-18.568 155.785-52.823v-86.216h-86.216C305.044 385.147 281.181 392 256 392z"
+                  data-original="#0f9d58"
+                />
+                <path
+                  fill="#31aa52"
+                  d="m139.131 325.477-86.308 86.308a260.085 260.085 0 0 0 22.158 25.235C123.333 485.371 187.62 512 256 512V392c-49.624 0-93.117-26.72-116.869-66.523z"
+                  data-original="#31aa52"
+                />
+                <path
+                  fill="#3c79e6"
+                  d="M512 256a258.24 258.24 0 0 0-4.192-46.377l-2.251-12.299H256v120h121.452a135.385 135.385 0 0 1-51.884 55.638l86.216 86.216a260.085 260.085 0 0 0 25.235-22.158C485.371 388.667 512 324.38 512 256z"
+                  data-original="#3c79e6"
+                />
+                <path
+                  fill="#cf2d48"
+                  d="m352.167 159.833 10.606 10.606 84.853-84.852-10.606-10.606C388.668 26.629 324.381 0 256 0l-60 60 60 60c36.326 0 70.479 14.146 96.167 39.833z"
+                  data-original="#cf2d48"
+                />
+                <path
+                  fill="#eb4132"
+                  d="M256 120V0C187.62 0 123.333 26.629 74.98 74.98a259.849 259.849 0 0 0-22.158 25.235l86.308 86.308C162.883 146.72 206.376 120 256 120z"
+                  data-original="#eb4132"
+                />
+              </svg>
+              Continue with Google
+            </button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
